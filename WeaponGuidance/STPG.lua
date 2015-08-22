de065fa053790e99f74fed5abc53373bcf361770
Blothorn <ianrsturdy@gmail.com>
2015-08-21 21:46:11 -0400

    Added STPG
    Still poorly tested, not completely implemented.

---
+++ WeaponGuidance/STPG.lua
@@ -0,0 +1,251 @@
+-- See Readme in git repository for parameter documentation
+-- Globals
+TargetBufferSize = 30
+AimPointMainframeIndex = 0
+NonAimPointMainframeIndex = nil
+
+TargetLists = {
+  AA = {
+    MainframeIndex = 0,
+      MinimumSpeed = 0,
+      MaximumSpeed = 250,
+      MinimumAltitude = -5,
+      MaximumAltitude = 99999,
+    MaximumRange = 1000,
+    TTT = 2
+  }
+}
+
+WeaponSystems = {
+-- Dart AA thumpers
+  {
+    Group = 1,
+    Type = 2,
+    TargetList = 'AA',
+    MaximumAltitude = 99999,
+    MinimumAltitude = -3,
+    MaximumRange = 800,
+    MinimumRange = 100,
+    FiringAngle = 60,
+    Speed = 150,
+    LaunchDelay = 0.3,
+    MinimumConvergenceSpeed = 50,
+    ProxRadius = nil,
+    SecantInterval = function(ttt) return math.ceil(40*ttt/2) end,
+    CullSpeed = 50,
+    TransceiverIndices = {0,1},
+    TTTIterationThreshold = 0.1,
+    TTTMaxIterations = 3,
+    SecantPoint = 'Position'
+  }
+}
+
+flag = 0
+
+-- Target buffers
+Targets = {}
+Missiles = {}
+
+function NewTarget(I, targetInfo)
+  return {
+    Position = { targetInfo.Position },
+    AimPoint = { targetInfo.AimPointPosition },
+    Index = 1,
+    Wrapped = 0,
+    Flag = flag,
+    Velocity = targetInfo.Velocity
+  }
+end
+
+function Length(vel)
+  return math.sqrt(vel.x^2 + vel.y^2 + vel.z^2)
+end
+
+function UpdateTargets(I)
+  -- Find priority targets
+  local nmf = I:GetNumberOfMainframes()
+  for tli, tl in pairs(TargetLists) do
+    local m = 0
+    if tl.MainframeIndex < nmf then
+      m = tl.MainframeIndex
+    end
+
+    -- Find qualifying target
+    tl.PresentTarget = nil
+    for tInd = 0, I:GetNumberOfTargets(m) - 1 do
+       local t = I:GetTargetInfo(m,tInd)
+       local speed = Length(t.Velocity)
+       local interceptPoint = t.Position + t.Velocity * tl.TTT
+       if (t.AimPointPosition.y > tl.MinimumAltitude)
+         and (t.AimPointPosition.y < tl.MaximumAltitude)
+         and (speed > tl.MinimumSpeed)
+         and (speed < tl.MaximumSpeed)
+         and (Length(I:GetConstructPosition() - interceptPoint) < tl.MaximumRange) then
+           tl.PresentTarget = t.Id
+           if not (Targets[t.Id]) then
+             Targets[t.Id] = NewTarget(I, t)
+           end
+           break
+       end
+    end
+  end
+
+  -- Cull unused targets
+  for i, t in ipairs(Targets) do
+    if t.Flag ~= flag then
+      Targets[i] = nil
+    end
+  end
+  for i, t in ipairs(Missiles) do
+    if t.Flag ~= flag then
+      Missiles[i] = nil
+    end
+  end
+  flag = flag+1 % 2
+
+  -- Update target info
+  local ami = 0
+  if AimPointMainframeIndex < nmf then
+    ami = AimPointMainframeIndex
+  end
+
+  for tInd = 0, I:GetNumberOfTargets(ami) - 1 do
+    local t = I:GetTargetInfo(ami, tInd)
+    if Targets[t.Id] then
+      local tar = Targets[t.Id]
+
+      if tar.Index <= TargetBufferSize then
+        tar.Index = tar.Index + 1
+      else
+        tar.Index = 1
+        tar.Wrapped = 1
+      end
+
+      tar.Velocity = t.Velocity
+      tar.Position[tar.Index] = t.Position
+      tar.AimPoint[tar.Index] = t.AimPointPosition
+    end
+  end
+end
+
+-- I -> Position -> Time -> Velocity
+function PredictVelocity(I, target, aimpoint, interval)
+  local aimpoint = 'Position'
+  -- Calculate the interval to use
+  if target.Wrapped == 0 then
+    interval = target.Index-1
+  else
+    interval = math.min(interval, TargetBufferSize-1)
+  end
+
+  local velocity = target.Velocity
+  if interval > 0 then
+    -- Use secant approximation to smooth
+      local oldPos = target[aimpoint][((target.Index - interval) % TargetBufferSize) + 1]
+      velocity = (target[aimpoint][target.Index] - oldPos) * (40 / interval)
+   end
+   return velocity
+end
+
+function FindConvergence(I, tPos, tVel, mPos, mSpeed, delay, minConv)
+   -- Calculates a time at which the missiles potential sphere intersects the target's track
+   local relativePosition = tPos - mPos
+   local distance = Length(relativePosition)
+   local targetAngle = I:Maths_AngleBetweenVectors(relativePosition, tVel)
+   local targetSpeed = Length(tVel)
+
+   -- Find time to earliest possible convergence point
+   local a = targetSpeed^2 - mSpeed^2
+   local b = 2 * targetSpeed * mSpeed * math.cos(math.rad(targetAngle))
+   local c = distance^2
+   local det = math.sqrt(b^2-4*a*c)
+   local ttt = 0
+
+   if det < 0 then
+      -- No converging path
+      I:Log('Negative determinant')
+      ttt = distance / minConv
+   else
+      local root1 = math.min((-b + det)/(2*a), (-b - det)/(2*a))
+      local root2 = math.max((-b + det)/(2*a), (-b - det)/(2*a))
+      if root1 > 0 then
+         ttt = root1
+      elseif root2 > 0 then
+         ttt = root2
+      else
+         I:Log('Negative ttt')
+         ttt = distance / minConv
+      end
+   end
+   return ttt
+end
+
+function PredictTarget(I, target, mPos, mSpeed, delay, Interval, minConv)
+   tPos = target.Position[Targets[tIndex].Index]
+   tVel = target.Velocity
+   aPos = target.AimPoint[Targets[tIndex].Index]
+   -- Find an initial ttt to find the secant width
+   local ttt = FindConvergence(I, tPos, tVel, mPos, mSpeed, delay, minConv)
+   -- Find the secant velocity
+   local secantVelocity = PredictVelocity(I, target, aPos, Interval(ttt+delay))
+
+   -- Use this to refine the TTT guess
+   ttt = FindConvergence(I, tPos, secantVelocity, mPos, mSpeed, minConv)
+   secantVelocity = PredictVelocity(I, target, aPos, Interval(ttt+delay))
+   return aPos + secantVelocity * (ttt+delay)
+end
+
+function Update(I)
+  I:ClearLogs()
+  UpdateTargets(I, target)
+
+  -- Aim and fire
+  for i = 0, I:GetWeaponCount() - 1 do
+    local w = I:GetWeaponInfo(i)
+    if WeaponSystems[w.WeaponSlot] then
+      local ws = WeaponSystems[w.WeaponSlot]
+      tIndex = TargetLists[ws.TargetList].PresentTarget
+      if Targets[tIndex] then
+        local tPos = PredictTarget(I, Targets[tIndex], w.GlobalPosition, ws.Speed, ws.LaunchDelay, ws.SecantInterval, ws.MinimumConvergenceSpeed)
+        tPos.y = tPos.y + 15
+        local vector = tPos - w.GlobalPosition
+        vector = vector / Length(vector)
+        I:AimWeaponInDirection(i, vector.x, vector.y, vector.z, w.WeaponSlot)
+
+        local angle = I:Maths_AngleBetweenVectors(I:GetConstructForwardVector(), vector)
+        if Length(w.GlobalPosition - tPos) < ws.MaximumRange and angle < 180 then
+          I:FireWeapon(i, w.WeaponSlot)
+        end
+      end
+    end
+  end
+
+  -- Guide missiles
+  for wsi = 0, 5 do
+    local ws = WeaponSystems[wsi]
+    if ws and ws.TransceiverIndices then
+      for ind, trans in ipairs(ws.TransceiverIndices) do
+        if I:GetLuaTransceiverInfo(trans).Valid then
+          for mi = 0, I:GetLuaControlledMissileCount(trans) - 1 do
+            local mInfo = I:GetLuaControlledMissileInfo(trans, mi)
+            if Missiles[mInfo.Id] == nil then
+              Missiles[mInfo.Id] = { Target = TargetLists[ws.TargetList].PresentTarget }
+            end
+
+            Missiles[mInfo.Id].Flag = flag
+            local target = Targets[Missiles[mInfo.Id].Target]
+            target.Flag = flag
+
+            if ws.ProxRadius and Length(target.AimPoint[target.Index] - mInfo.Position) < ws.ProxRadius then
+              I:DetonateLuaControlledMissile(trans,m)
+            end
+
+            local mSpeed = math.max(Length(mInfo.Velocity), ws.Speed)
+            tPos = PredictTarget(I, target, mInfo.Position, ws.Speed, 0, ws.SecantInterval, ws.MinumumConvergenceSpeed)
+            I:SetLuaControlledMissileAimPoint(trans, mi, tPos.x, math.min(ws.MaximumAltitude, math.max(tPos.y, ws.MinimumAltitude)),tPos.z)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ WeaponGuidance/STPG.md
@@ -0,0 +1,45 @@
+# Notes
+This partially implements a comprehensive weapon system built around a target prediction algorithm using secant approximations to improve resolution of velocity predictions (at close range) and smoothing of erratic behaviour (at long range). It performs relatively poorly against targets turning steadily, such as orbiters; this can be mitigated with low secant width settings, and more precise solutions are under consideration.
+
+Because the game scales the number of ticks per second when the game speed is changed, this calculates incorrect target velocities and thus intercept points when the game is slowed down manually (as opposed to simply lagging).
+
+# Parameter documentation
+## Globals
++ `TargetBufferSize`: The number of ticks of position information kept. Higher numbers allow greater smoothing (accompanied by slower response to manuevers) at the expense of memory use. 40 ticks = 1 second.
++ `AimPointMainframeIndex`: A mainframe using aimpoint.
++ `NonAimPointMainframeIndex`: A mainframe not using aimpoint.
+
+## Target lists
+To account for the target prioritization card's ambivalence to target direction, range, and altitude, weapons controlled by this code take their targets from filtered target lists. `TargetLists` is an array of such lists.
++ `MainframeIndex`: The preferred mainframe to use for target prioritization. Presently wrong if mainframes are damaged.
++ `MinimumSpeed`: The minimum speed of the target.
++ `MaximumSpeed`: The maximum speed of the target.
++ `MinimumAltitude`: The minimum altitude of the aimpoint.
++ `MaximumAltitude`: The maximum altitude of the aimpoint.
++ `MaximumRange`: The maximum range.
++ `TTT`: This flighttime will be used to calculate an intercept point and that checked against the maximum range; it should usually be based on the flight time to maximum range; setting it slightly high will do a better job of pre-aligning weapons so that they can fire when the target enters range (an increased `TTT` is preferred ot an increased `MaximumRange` for this use as the latter will also allow targets just outside true maximum range but not closing)
++ `Aimpoint`: Whether to use aimpoint. `0` will never use it, `1` will use it and select a different target if the present one violates the altitude restrictions. `2` will try both aimpoint and non-aimpoint mainframes for a valid target position.
++ `PresentTarget`: The index of the present target (set automatically).
+
+## WeaponSystems
+`WeaponSystems` is an array indexed by the weapon group number containing information specific to the weapons in that group. This code assumes that all components in a system are practically homogenous; assign different types of weapons to different weapon groups. And for now, only put one group of weapons on each turret. Only missiles and lasers are presently supported.
+
++ `Group`: Weapon group (0-5).
++ `Type`: 0 for lasers, 1 for cannon, 2 for missiles. For turreted weapons, use the weapon type on the turret.
++ `TargetList`: The virtual mainframe that will be used for targetting.
++ `MinimumAltitude`: The minimum altitude of the aimpoint. Invalid aimpoints will be projected up to this altitude if it seems within the vehicle's bounding box.
++ `MaximumAltitude`: The maximum altitude of the aimpoint. Invalid aimpoints will be projected down to this altitude if it seems within the vehicle's bounding box.
++ `MinimumRange`: The minimum range to the target. Useful primarily for off-axis missiles.
++ `MaximumRange`: Maximum range to the intercept point (calculated using speed/delay TTT).
++ `FiringAngle`: Maximum deviation at which to fire. Future plans to allow for a vector3 to better accomodate missiles on turrets with no/limited elevation.
++ `Speed`: Expected mean speed. Used for calculations before flight time is calculated. Important for missiles, as their `WeaponInfo.Speed` is a fixed and inaccurate 100.
++ `LaunchDelay`: Expected delay incurred (relative to a launch at `Speed`) during launch (added flat to ttt when calculating intercept position for aiming and range calculations).
++ `MinimumConvergenceSpeed`: A minimum convergence speed when calculating intercept points. Useful for getting missiles to follow a fast target in the hope that he will turn back in range (with higher values sticking closer to a pursuit course), but too high a value will lead to undercorrection in otherwise valid intercept solutions.
++ `ProxRadius`: The distance from the target at which a missile will be manually detonated.
++ `ProxAngle`: The angle to the aimpoint required for a proximity detonation (for narrow frags).
++ `SecantInterval`: `Time -> Int`. The number of ticks over which to average velocity as a function of the time to target. This should usually converge to (0,0); `math.ceil(40*ttt)` works well.
++ `CullSpeed`: The speed below which missiles will be manually detonated. Useful for preserving system resources (particularly with thumpers, which are of little use after slowing down).
++ `TransceiverIndices`: The indices of the controlled Lua missiles. These will be wrong if low indices are damaged, but until `GetLuaTransceiverInfo` is fixed I cannot do anything about that.
++ `TTTIterationThreshold`: If calculating an intercept point yields a proportional change in TT above this threshold, it will be recalculated with the new TTT.
++ `TTTMaxIterations`: The maximum number of iterations to converge on a TTT. 0 will use `TargetInfo.Velocity`. These are potentially expensive, so keep low.
++ `UseAimpointVelocity`: Whether to calculate the velocity from the aimpoint or the vehicle position. Aimpoint velocity is noisier but more precise, particularly when the aimpoint target is far from the vehicle position. It does jump noticeably whenever the aimpoint is destroyed; best used for weapons firing distinct volleys (travel time less than reload time).
